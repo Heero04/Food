cf21c28a536f4cce9397bc3aebe960f0b9faa3e1
Heero <lawrencedavis1010@gmail.com>
2019-01-26 22:55:16 -0500

    Updates

---
+++ precache-manifest.9f8cb122d35d79572650b1d56312be39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f19d28d6cdb6e3b47c633e5271ffc477",
+    "url": "/Food/static/media/background_search_mobile.f19d28d6.jpg"
+  },
+  {
+    "revision": "ded51103c6bd13cac46938406300a595",
+    "url": "/Food/static/media/background_search_desktop.ded51103.jpg"
+  },
+  {
+    "revision": "eb340855e84dbc924572",
+    "url": "/Food/static/js/runtime~main.eb340855.js"
+  },
+  {
+    "revision": "fcbd04c3af9bbaa28db6",
+    "url": "/Food/static/js/main.fcbd04c3.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/Food/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "fcbd04c3af9bbaa28db6",
+    "url": "/Food/static/css/main.c97ecb49.chunk.css"
+  },
+  {
+    "revision": "2371138dd3660c07e121c5dcb1afa65b",
+    "url": "/Food/index.html"
+  }
+];
